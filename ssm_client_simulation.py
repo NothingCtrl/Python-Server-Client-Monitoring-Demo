4e852437ee9b04354c6cf0ae97493e784e058866
NothingCtrl <nothingctrl@gmail.com>
2016-12-08 18:45:50 +0700

    Update encoding...

--- ssm_client_simulation.py
+++ ssm_client_simulation.py
@@ -6,6 +6,18 @@ server_address = socket.gethostname()
 server_port = 9999
 
 
+def xor_crypt_string(data, key='awesomepassword', encode=False, decode=False):
+    from itertools import izip, cycle
+    import base64
+
+    if decode:
+        data = base64.decodestring(data)
+    xored = ''.join(chr(ord(x) ^ ord(y)) for (x, y) in izip(data, cycle(key)))
+    if encode:
+        return base64.encodestring(xored).strip()
+    return xored
+
+
 def client_service(count):
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # connect to server
@@ -13,24 +25,34 @@ def client_service(count):
     c.connect((server_address, server_port))
     print "Connected"
 
-    if count != 5:
+    if count == 2:
+        print "Client 2 make connection but not send any data..."
+
+    if count != 5 and count != 2:
+        encoded = xor_crypt_string("This is data 1 send from client: %d" % count, encode=True)
+        packet_1 = encoded[:15]
+        packet_2 = encoded[15:]
         time.sleep(1)
-        c.send("This data 1 send from client: %d" % count)
+        c.send(packet_1)
+        print "%d - Packet sended [1]: %s" % (count, packet_1)
+        time.sleep(0.1)
+        c.send(packet_2)
+        print "%d - Packet sended [2]: %s" % (count, packet_2)
         time.sleep(1)
-        c.send("This data 2 send from client: %d" % count)
+        c.send(xor_crypt_string("This data 2 send from client: %d" % count, encode=True))
 
     if count == 5:
         print "Count = 5, will send data after 45s"
         time.sleep(45)
-        c.send("Count 5 send data after 45s")
+        c.send(xor_crypt_string("Count 5 send data after 45s", encode=True))
 
     if count == 7:
-        c.send("Client going sleep to test if thread terminated time out")
+        c.send(xor_crypt_string("Client going sleep to test if thread terminated time out", encode=True))
         time.sleep(30)
-        c.send("If this message reach sever, thread not terminated after time out")
+        c.send(xor_crypt_string("If this message reach sever, thread not terminated after time out", encode=True))
 
-i = 0
-while i <= 100:
+i = 1
+while i <= 3:
     print i
     threading.Thread(target=client_service, args=(i,)).start()
     time.sleep(.2)
--- ssm_server.py
+++ ssm_server.py
@@ -4,32 +4,80 @@ import logging
 import time
 
 host = socket.gethostname()
+# listen port
 port = 9999
+# encode / decode key
+key = 'awesomepassword'
 
 
 class Server(object):
-    def __init__(self, listen_port):
+    def __init__(self, listen_port, pass_key):
         self.host = socket.gethostname()
         self.port = listen_port
         self.count_thread = 0
-
-    @staticmethod
-    def client_hande(client, timeout=2, thread_time=5):
+        self.key = pass_key
         # timeout: set time close of connection if no data receive from client
+        self.socket_timeout = 2
+
+    def xor_crypt_string(self, data, encode=False, decode=False):
+        """
+        Encode / decode data between client and server
+        :param data:
+        :param encode:
+        :param decode:
+        :return:
+        """
+        from itertools import izip, cycle
+        import base64
+
+        if decode:
+            data = base64.decodestring(data)
+        xored = ''.join(chr(ord(x) ^ ord(y)) for (x, y) in izip(data, cycle(self.key)))
+        if encode:
+            return base64.encodestring(xored).strip()
+        return xored
+
+    def recv_timeout(self, client):
         # timeout reset (recouting) everytime have data from client
-        client.settimeout(timeout)
+        # client.settimeout(self.socket_timeout)
+        client.setblocking(0)
+        begin = time.time()
+        end_time = begin + self.socket_timeout + 0.5
+        total_data = []
+        # sleep_count = 0
+        while time.time() < end_time:
+            if total_data and time.time() - begin > self.socket_timeout:
+                break
+            try:
+                data = client.recv(102400)
+                if data:
+                    total_data.append(data)
+                else:
+                    # sleep for sometime to indicate a gap
+                    time.sleep(0.1)
+            except:
+                pass
+        # join all parts to make final string
+        return ''.join(total_data)
+
+    def client_hande(self, client, thread_count, thread_time=5):
         # thread_time: maximum run time of thread, note that if timeout > thread_time,
         # data = client.recv(102400) still waiting in first loop
         end_time = time.time() + thread_time
         while time.time() < end_time:
             # every timeout on 'client.recv(102400)' while will loop,
             # so this will loop 2 times if no data send / no connection from client side, with default value 2,5
-            data = client.recv(102400)
+            data = self.recv_timeout(client)
+            # data = client.recv(102400)
             if data:
-                print "Data from client: ", data
-            # break
+                deccoded_data = self.xor_crypt_string(data, decode=True)
+                # save data to database
+                print deccoded_data
+                # no need wating more data
+                client.close()
+                break
         client.close()
-        print "End connection..."
+        print "End connection... ", thread_count
 
     def run(self):
         print "Server start, listen port: ", self.port
@@ -40,9 +88,9 @@ class Server(object):
             try:
                 client, address = serversocket.accept()
                 self.count_thread += 1
-                threading.Thread(target=self.client_hande, args=(client,)).start()
+                threading.Thread(target=self.client_hande, args=(client, self.count_thread)).start()
             except Exception as e:
                 logging.exception(e)
 
 # start server
-Server(9999).run()
+Server(9999, key).run()
